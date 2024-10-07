af4bfff2a2378c06c717a8ebd8fc9fc43cd11ef1
YasinLaskar <yasinlaskar05@gmail.com>
2024-10-08 01:26:27 +0530

    managed some files

--- app..js
+++ app..js
@@ -1,15 +1,34 @@
 
-let slideIndex = 0;
-showSlides();
+let button=document.querySelector("#btn");
+button.style.borderRadius='10px';
+button.style.width='200px';
+button.style.height='50px';
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+
+
+
+button.addEventListener("click" , function(){
+
+let h1=document.querySelector("h1");
+let randomColor=randomCOlor();
+
+h1.innerText=randomColor;
+h1.style.color=randomColor;
+
+let div=document.querySelector("div");
+div.style.background=randomColor;
+console.log("color updated");
+});
+    
+
+  
+
+
+function randomCOlor(){
+
+    let red=Math.floor(Math.random()*255);
+    let green=Math.floor(Math.random()*255);
+    let blue=Math.floor(Math.random()*255);
+    let color=`rgb( ${red} ,${green} ,${blue})`;
+    return color;
 }
